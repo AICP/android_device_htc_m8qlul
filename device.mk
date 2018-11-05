52e965415d9789e56e5085d26f2074c0f09018bc
Christos Papageorgiou (root-expert) <root.expert.xda@gmail.com>
2018-11-05 13:32:51 +0100

    m8qlul: Remove GesturesHandler
    * This is not buildable after switching to different base. Kick it for
    now
    
    Change-Id: I63037804137de8eb5ceac456393b7773e8f0fc5b

--- device.mk
+++ device.mk
@@ -184,10 +184,6 @@ PRODUCT_PACKAGES += \
     org.simalliance.openmobileapi \
     org.simalliance.openmobileapi.xml
 
-# Gestures
-PRODUCT_PACKAGES += \
-    GestureHandler
-
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
